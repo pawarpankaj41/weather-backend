4c8dcaba3a08514ec722430f7e0ba32de4e33b5f
Pankaj Pawar <pawarpankaj41@gmail.com>
2023-09-15 21:43:58 +0530

    added the models in the admin file

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Region, Parameter, WeatherData
+
+admin.site.register(Region)
+admin.site.register(Parameter)
+admin.site.register(WeatherData)
+
