628b21b75b34312ee2c6c093fe03a459b3f37447
Pankaj Pawar <pawarpankaj41@gmail.com>
2023-09-15 21:47:13 +0530

    modified the WeatherData model added new fields in the model, used choices to save month and season values

--- base/models.py
+++ base/models.py
@@ -16,6 +16,9 @@ class AppBaseModel(models.Model):
 
 
 class Region(AppBaseModel):
+    """
+    The Region model to save the regions.
+    """
     title = models.CharField(max_length=255)
     parsing_name = models.CharField(max_length=255)
 
@@ -23,6 +26,9 @@ class Region(AppBaseModel):
         return self.title
 
 class Parameter(AppBaseModel):
+    """
+    The Parameter to save the weather parameters.
+    """
     title = models.CharField(max_length=255)
     parsing_name = models.CharField(max_length=255)
 
@@ -30,12 +36,41 @@ class Parameter(AppBaseModel):
         return self.title
 
 
+class MonthChoices(models.IntegerChoices):
+    january =  1, "January"
+    february = 2, "February"
+    march =  3, "March"
+    april =  4, "April"
+    may =  5, "May"
+    june =  6, "June"
+    july =  7, "July"
+    august = 8 , "August"
+    september =  9, "September"
+    october =   10, "October"
+    november = 11, "November"
+    december = 12, "December"
+
+class SeasonChoices(models.IntegerChoices):
+    winter = 1, "Winter"
+    spring = 2, "Spring"
+    summer = 3, "Summer"
+    autumn = 4, "Autumn"
+
+
 class WeatherData(AppBaseModel):
+    
+
+    class RecordTypeChoices(models.IntegerChoices):
+        monthly = 1, "Monthly"
+        seasonal = 2, "Seasonal"
+        annual = 3, "Annual"
+        
     year = models.IntegerField()
-    month = models.CharField(max_length=3)
+    month = models.PositiveIntegerField(choices=MonthChoices.choices, null=True)
+    season = models.PositiveIntegerField(choices=SeasonChoices.choices, null=True)
+    record_type = models.PositiveIntegerField(choices=RecordTypeChoices.choices, default=RecordTypeChoices.monthly)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
-    value = models.FloatField()
-
+    value = models.FloatField(null=True)
 
     
