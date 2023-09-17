afb644c6d6cf81ff72580e6eecf6d52ca65241ff
Pankaj Pawar <pawarpankaj41@gmail.com>
2023-09-17 09:46:40 +0530

    changed the ALLOWED_HOSTS list in the settings to reflect of server ip

--- weather_backend/settings.py
+++ weather_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["16.171.137.237","localhost"]
 
 
 # Application definition
